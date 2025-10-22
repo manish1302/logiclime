fcff46035e7b0b228ba3a0954effa2ddb5d69e79
manish@1302 <manishjadav1302@gmail.com>
2025-10-23 02:03:07 +0530

    reverting deployment testing

--- client/src/Containers/Landingpage.jsx
+++ client/src/Containers/Landingpage.jsx
@@ -70,7 +70,8 @@ const Landingpage = () => {
           <div className="features-header">
             <div className="f-heading">What sets us apart?</div>
             <div className="f-desc">
-              deployment testing...
+              At <span className="">Logic Lime</span> we not only offer a
+              service, we also offer love and experience.
             </div>
           </div>
           <div className="features-content">
